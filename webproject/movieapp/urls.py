afd3d452a747fb32a61930524f5d3f846155674c
EbinIttira <99314440+EbinIttira@users.noreply.github.com>
2022-02-15 23:43:21 +0530

    Delete urls.py

--- webproject/movieapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from. import views
-app_name='movieapp'
-urlpatterns = [
-    path('',views.display,name='display'),
-    path('movie/<int:movie_id>/',views.detail,name='detail'),
-    path('add/',views.add,name='add'),
-    path('update/<int:id>/',views.update,name='update'),
-]
